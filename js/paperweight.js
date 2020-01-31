3a8c0900b4a092963a5d9d2a70e9d780f8ed4ef0
Kyle Johnson <johnsokm5070@uwec.edu>
2020-01-30 22:08:14 -0600

    add progress bar, more calculations

--- js/paperweight.js
+++ js/paperweight.js
@@ -1,22 +1,24 @@
 var timeFormat = 'hh:mm a';
 
-var startTime = '7:00am';
-var endTime = moment(startTime, timeFormat).add(12, 'hours').format(timeFormat)
-
 var paidHours = '';
 var salary = '';
 var wage = '';
 var pay = '';
 
+var startTime;
+var endTime;
+var currentTime;
+
+var percentComplete;
+var totalTime;
+var timeElapsed;
+
+var progressBar;
+
 $(function() {
-    $('#start').timepicker({
-        'minTime': startTime,
-        'maxTime': endTime
-    });
+    $('#start').timepicker();
 
     $('#end').timepicker({
-        'minTime': startTime,
-        'maxTime': endTime,
         'showDuration': true
     });
     
@@ -31,7 +33,7 @@ $(function() {
     });
     
     // comment out line to show all at once
-    $('#paySection, .salary, .hourly, #second').hide();
+    $('#paySection, .salary, .hourly, #second, #third').hide();
     
     $('input[name=pay]').change(function() {
         pay = $(this).val();
@@ -76,8 +78,17 @@ function updateRange() {
 
 function calculateShift() {
     if ($('#start').val() && $('#end').val()) {
-        var startTime = moment($('#start').val(), timeFormat);
-        var endTime = moment($('#end').val(), timeFormat);
+        startTime = moment($('#start').val(), timeFormat);
+        endTime = moment($('#end').val(), timeFormat);
+
+        // adjust for overnight shifts
+        if (endTime.isBefore(startTime)) {
+            endTime = endTime.add(1, 'days');
+        }
+
+        startClock();
+        $('#third').show();
+
         var duration = moment.duration(endTime.diff(startTime));
         var paidDuration = moment.duration(endTime.diff(startTime));
         $('#hours').text('Total shift length: ' + duration.asHours() + ' hours');
@@ -86,6 +97,84 @@ function calculateShift() {
         paidHours = paidDuration.asHours()
         $('#paid').val(paidHours);
         $('#paidHours').text('Total hours paid: ' + paidHours + ' hours')
+    }
+}
+
+function startClock() {    
+    if (progressBar === undefined) {
+        createProgressBar()
+        updateTime();
+        setInterval(updateTime, 1000);
+    }
+}
+
+function createProgressBar() {
+    progressBar = new ProgressBar.SemiCircle('#progressbar', {
+      strokeWidth: 4,
+      easing: 'easeInOut',
+      duration: 1000,
+      color: '#fff',
+      trailColor: '#ccc',
+      trailWidth: 1,
+      svgStyle: null
+    });
+}
+
+function updateTime() {
+    currentTime = moment();
+    $('#currentTime').text('Current Time: ' + currentTime.format('h:mm:ss a'));
+    calculatePercentComplete();
+    calculateTimeWorked();
+    calculateProfit()
+}
+
+function calculatePercentComplete() {
+    timeElapsed = currentTime.unix() - startTime.unix();
+    totalTime = endTime.unix() - startTime.unix();
+    percentComplete = Math.trunc((timeElapsed / totalTime) * 10000) / 100;
+
+    if (percentComplete < 0) {
+        $('#percentComplete').text('Your shift has not started yet.');
+        progressBar.animate(0);
+    } else if (percentComplete > 100) {
+        $('#percentComplete').text('Your shift is over. Enjoy your day!');
+        progressBar.animate(1);
+    } else {
+        $('#percentComplete').text('Percentage Complete: ' + percentComplete + '%');
+        progressBar.animate(timeElapsed / totalTime);
+    }
+}
+
+function calculateTimeWorked() {
+    var hours = Math.trunc(timeElapsed / 3600);
+    var minutes = Math.trunc((timeElapsed - (hours * 3600)) / 60);
+    var timeWorked = '';
+    if (hours > 0) {
+        timeWorked += hours + ' hour';
+        if (hours != 1) {
+            timeWorked += 's'
+        }
+    }
+    timeWorked += ' ';
+    if (minutes > 0) {
+        timeWorked += minutes + ' minute';
+        if (minutes != 1) {
+            timeWorked += 's'
+        }
+    }
+    $('#timeWorked').text('Time Worked: ' + timeWorked);
+}
+
+function calculateProfit() {
+    var wagePerSecond = wage / 3600;
+    var profit = Math.trunc(wagePerSecond * timeElapsed * 100) / 100;
+    var maxProfit = Math.trunc(wage * paidHours);
+    if (percentComplete < 0) {
+        $('#profit').text('Profit: $0.00');
+    } else if (percentComplete > 100) {
+        $('#profit').text('End of day profit: $' + maxProfit);
+    } else {
+        $('#profit').text('Profit thus far: $' + profit.toFixed(2));
     }
 }
 
@@ -133,12 +222,14 @@ function calculateSalaryData(secondary = false) {
 function updateConversions() {
     var dayEarnings = Math.trunc(wage*paidHours)
     var weeklyEarnings = Math.trunc(dayEarnings*5)
-    var monthlyEarnings = Math.trunc(salary/12)
+    var monthlyEarnings = Math.trunc(weeklyEarnings*4)
+    var paycheckEstimate = Math.ceil(salary/24 * 100)/100
     var truncatedWage = Math.trunc(wage*100)/100
     
-    $('#dayEarnings').text('$' + truncatedWage + ' * ' + paidHours + ' hours' + ' = $' + dayEarnings + '/day');
-    $('#weeklyEarnings').text('Weekly earnings: $' + weeklyEarnings + '/week');
-    $('#monthlyEarnings').text('Monthly earnings: $' + monthlyEarnings + '/month');
+    $('#dayEarnings').text('Daily earnings ($' + truncatedWage + ' * ' + paidHours + ') = $' + dayEarnings);
+    $('#weeklyEarnings').text('Weekly earnings (x5) = $' + weeklyEarnings);
+    $('#monthlyEarnings').text('Monthly earnings (x20) = $' + monthlyEarnings);
+    $('#paycheckEstimate').text('Bi-weekly paycheck: $' + paycheckEstimate);
     
     $('#conversions').show()
 }
